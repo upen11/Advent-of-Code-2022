e4648d28326060ebd59726b7c440cc57528b2bf6
Upendra Panwar <upendra.xl8@gmail.com>
2022-12-02 00:07:08 +0530

    Day 1

---
+++ Advent-of-Code-2022/src/dev/upendra/aoc22/AoC2022Core.java
@@ -0,0 +1,10 @@
+package dev.upendra.aoc22;
+public class AoC2022Core {
+
+	public static void main(String[] args) {
+		new Day01();
+		// new Day02();
+
+	}
+
+}
---
+++ Advent-of-Code-2022/src/dev/upendra/aoc22/Day01.java
@@ -0,0 +1,48 @@
+package dev.upendra.aoc22;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Day01 extends AOCBaseCode {
+
+	public Day01() {
+		super("1");
+	}
+	
+	@Override
+	void solve(List<String> input) {
+//		System.out.println(input);
+		List<Integer> calories = new ArrayList<>();
+		
+		int sum = 0;
+		int max = 0;
+		int elfNum = 0;
+		int ans = 0;
+		for(String i : input) {
+			if(!i.trim().equals("")) {
+				sum += Integer.parseInt(i);
+			}
+			else {
+				calories.add(sum);
+				elfNum++;
+				if(sum > max) {
+					max = sum;
+					ans = elfNum;
+				}
+				sum = 0;
+			}
+		}
+		
+		lap(max);
+
+		Collections.sort(calories);
+		
+		// descending
+		// calories.sort((a, b) -> b - a);
+		
+		int n = calories.size()-1;
+		
+		lap(calories.get(n) + calories.get(n-1) + calories.get(n-2));
+	}
+}
